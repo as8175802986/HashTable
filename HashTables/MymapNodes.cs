523fd48aa931c4c3c1e80f1d4421f8d1f48923a6
“Arunesh <as8175802986@gmail.com>
2021-11-25 18:53:00 +0530

    [Arunesh] Added: code for Hash Tables

---
+++ HashTables/MymapNodes.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+    public class MymapNodes<K,V>
+    {
+        private readonly int size;
+        private  LinkedList<KeyValue<K, V>>[] items;
+
+        public MymapNodes(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayaposition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayaposition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach ( KeyValue<K, V> items in linkedList)
+            {
+                if (items.Key.Equals(key))
+                {
+                    return items.Value;
+                }
+
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayaposition(key);
+
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() {Key  = key, Value  = value };
+            linkedList.AddLast(item);
+
+        }
+        public void Remove(K key)                                                    //the remove method
+        {
+            int position = GetArrayaposition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;                                                  //declaring itemfound variable to be boolean false.
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)                          //checking if the input key is already present.
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;                                                //if yes, change the boolean to false 
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)         //this class takes the input as the integer position in range of array items indexes.    
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];             //checks whether there exist any value in that psoition. assigned to linkedlist obj.
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();                   // if there isn't any value, then create the linked list obj in that respective position
+                items[position] = linkedList;
+            }
+            return linkedList;                                                       //return the linkeddlist obj.
+
+        }
+    }
+    
+}
---
+++ HashTables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Hash table");
+            MymapNodes<string, string> hash = new MymapNodes<string, string>(5);
+            hash.Add("0", "Arunesh");
+            hash.Add("1", "Harshit");
+            hash.Add("2", "Gaurav");
+            hash.Add("3", "Raj");
+            hash.Add("4", "Anshu");
+            hash.Add("5", "Rishu");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value:" + hash5);
+
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2th index value:" + hash2);
+            Console.ReadLine();
+        }
+    }
+}
