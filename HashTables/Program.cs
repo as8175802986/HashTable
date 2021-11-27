edb20e23b0c2b0fd0bf33e9fb98f2552057cb011
“Arunesh <as8175802986@gmail.com>
2021-11-27 14:42:13 +0530

    [Arunesh] Added:code to Add BST

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -8,33 +8,27 @@ namespace HashTables
 {
     class Program
     {
-        public static void CountingWordFrequency(string sentence)            //this static method counts thr frquency of the words.
-        {
-            MymapNodes<string, int> hashtable = new MymapNodes<string, int>(10);      //declaring the key and value data types of the Mymapnodde class.
-            string[] words = sentence.Split();                //splitting the sentence otherwise it wont be possible to generate keys.
-            foreach (string word in words)
-            {
-                if (hashtable.Exists(word))                    //word is the "key"
-                {
-                    hashtable.Add(word, hashtable.Get(word) + 1);           //if it exists. get the value of the linkedlist object and invrement its value.
-                }
-                else
-                {
-                    hashtable.Add(word, 1);
-                }
-            }
-            Console.WriteLine("Displaying after add operation\n");
-            hashtable.Display();
-            hashtable.Remove("avoidable");                                     //display after removing the key value pair with key avoidable
-            Console.WriteLine("Displaying after removing avoidable.\n");
-            Console.WriteLine("----------------------------------------");
-            Console.ReadLine();
-        }
         static void Main(string[] args)
         {
-            CountingWordFrequency("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations");
-
-
+            Console.WriteLine("Welcome To Binary Search Tree ");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(72);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(31);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+            binarySearch.Display();
+            binarySearch.GetSize();
+            bool result = binarySearch.IfExists(67, binarySearch);
+            Console.WriteLine(result);
+            Console.ReadLine();
 
         }
     }
